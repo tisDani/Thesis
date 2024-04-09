85f85a7f7883b0eff28d39e8e0a2ace47218f026
tisDani <127032149+tisDani@users.noreply.github.com>
2024-04-09 14:20:21 +0200

    Table improved

--- thesis.py
+++ thesis.py
@@ -12,44 +12,49 @@ import random
 from colorama import Back, Back, Style, init
 init(autoreset=True)
 
+random.seed(10)
+
 class Card:
 
     def __init__(self, number, colour, text):
         self.number = number
         self.colour = colour   
-        #self.player = player #sub position, deck
-        #self.position = position   #hand, discard, table, Player, deck, middle
         self.text = text
 
-    def show(self):
-        print('{} {}  '.format(self.number , self.colour))
+    #def show(self):
+    #    print('{} {}  '.format(self.number , self.colour))
 
     def paint(self):     
         print(self.text +'{} '.format(self.number), end =" ")
 
-    def move(self, new_player, new_position):
-         self.player = new_player
-         self.position = new_position
-  
+    def paint_big(self):
+        print(self.text + '       \n   {}   \n      .'.format(self.number))
+
 
 class Player: 
     hand = None
     played = None
     discarded = None
+    store = None
     name = None
 
     def __init__(self, name):
         self.hand = Deck('hand ' + name)
         self.played = Deck('played ' + name)
         self.discarded = Deck('discarded ' + name)
+        self.store = Deck('store ' + name)
         self.name = name
 
     def draw(self, deck, num):
-        deck.deal(self.hand, num)
-        #take n cards from deck
+        deck.deal(self.hand, num)  #take n cards from deck
     
-    #def pshow(self):
-    #    print("{: >20} {: >20} {: >20}".format(printdeck(self.hand), 2, 3))
+    def pshow(self):
+        print("\n {} {: >20} {: >20} {: >20} ".format(self.name,'Hand', 'Discard', 'Store'))
+        self.hand.printdeck()
+        self.discarded.printdeck()
+        self.store.printdeck()
+        print('\n')
+        #print('{: >20} {: >20} {: >20}'.format(self.hand.printdeck(), 'hi', 'ho'))
 
 class Deck:
     cards = None
@@ -63,7 +68,12 @@ class Deck:
         colours = [(2,'purple',Back.CYAN), (2, 'red',Back.RED), (2, 'yellow',Back.YELLOW), (3, 'blue',Back.BLUE), (3, 'orange',Back.MAGENTA), (4, 'green',Back.GREEN), (5, 'black',Back.WHITE)]
         for colour in colours:
             for i in range(0,colour[0]):
-                self.cards.append(Card(colour[0], colour[1], colour[2]))    
+                self.cards.append(Card(colour[0], colour[1], colour[2])) 
+
+    def build_big(self):
+        colours =  [(2,'purple',Back.CYAN), (2, 'red',Back.RED), (2, 'yellow',Back.YELLOW), (3, 'blue',Back.BLUE), (3, 'orange',Back.MAGENTA), (4, 'green',Back.GREEN), (5, 'black',Back.WHITE)]
+        for colour in colours:
+            self.cards.append(Card(colour[0], colour[1], colour[2]))   
 
     def shuffle(self):
         random.shuffle(self.cards)
@@ -76,14 +86,13 @@ class Deck:
         return len(self.cards)
     
     def printdeck(self):
-        print(self.name, end = ':      ')
+        print('.', end = '                 ')
         for i in range(0, self.length()):
             self.cards[i].paint()
-        print('\n')
 
 
 def move(fromm, to, item):
-    item.paint()
+    #item.paint()
     fromm.remove(item) #list.remove(x): x not in list
     to.append(item)
 
@@ -94,6 +103,7 @@ def mul_move(fromm, to, items):
  
 class HanamikojiEngine:
     deck = None
+    big_deck = None
     player1 = None
     player2 = None
     currentPlayer = None
@@ -101,31 +111,53 @@ class HanamikojiEngine:
 
     def __init__(self):
         self.deck = Deck('stack')
+        self.big_deck = Deck('big')
         self.player1 = Player("Player 1")
         self.player2 = Player("Player 2")
         self.currentPlayer = self.player1
 
     def start(self):
         self.deck.build()
-        self.player1.draw(self.deck, 2)
+        self.big_deck.build_big()
+        self.player1.draw(self.deck, 5)
         self.player2.draw(self.deck, 5)
 
     def show_table(self):
         print('\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-        self.player1.hand.printdeck()
-        self.player1.discarded.printdeck()
-        self.player1.played.printdeck()
-        self.player2.hand.printdeck()
-        self.player2.discarded.printdeck()
-        self.player2.played.printdeck()
+        self.player1.pshow()
+        print('\n')
+        game.big_deck.printdeck()
+        print('\n')
+        self.player2.pshow()
         self.deck.printdeck()
-        print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+        print('\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
         
+
+    def trade2x2(self, fromm, to, a1, a2, b1, b2):
+        pass
+
+    def trade3x1(self, fromm, to, a, b, c):
+        pass
+
+    def discard(self, fromm, a, b):
+        mul_move(fromm.hand.cards, fromm.discarded.cards, [a,b])
+
+    def store(self, fromm, a):
+        move(fromm.hand.cards, fromm.store.cards, a)
 
 game = HanamikojiEngine()
 game.start()
+#game.show_table()
+game.discard(game.player1, game.player1.hand.cards[0],  game.player1.hand.cards[1])
+#game.show_table()
+game.store(game.player2, game.player2.hand.cards[3])
 game.show_table()
 
+
+
+
+#game.player1.pshow()
+#game.player1.hand.cards[2].paint_big()
 #print('---------------------------1----------------------------------------------')
 
 #print('~~~~~~~~~~~~~~~~~~~~~~~~~~2~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
