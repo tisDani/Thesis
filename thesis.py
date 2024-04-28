22cc44ed6a1d4f0082c98b5813bf4709a661f797
tisDani <127032149+tisDani@users.noreply.github.com>
2024-04-28 16:20:29 +0200

    Partially functioning Monte Carlo player

--- thesis.py
+++ thesis.py
@@ -1,5 +1,7 @@
 #references: https://docs.replit.com/tutorials/python/build-card-game-pygame
 import random
+from itertools import combinations
+import copy
 from colorama import Back, Back, init
 init(autoreset=True)
 
@@ -38,7 +40,7 @@ class Player:
         self.discarded = Deck('discarded ' + name)
         self.store = Deck('store ' + name)
         self.name = name
-        self.moves = [0, 0, 0, 0]
+        self.moves = [['Store', 0, 1], ['Discard', 0, 2], ['Trade 3x1', 0, 3], ['Trade 2x2', 0, 4]]
 
     def draw(self, deck, num):
         deck.deal(self.hand, num)  #take n cards from deck
@@ -101,6 +103,11 @@ class Deck:
         for card in self.cards:
             self.score[card.position - 1] +=1
 
+    def deck_id(self):
+        ids = []
+        for card in self.cards:
+            ids.append(card.id)
+        return ids
 
 def move(from_deck, to_deck, card_id):  
     card = i_d(from_deck, card_id)
@@ -110,6 +117,7 @@ def move(from_deck, to_deck, card_id):
         to_deck.count()
 
 def i_d(deck, iD):
+    print(iD)
     cards = (card for card in deck.cards 
         if card.id == iD)
     return next(cards)
@@ -117,6 +125,7 @@ def i_d(deck, iD):
 def mul_move(from_deck, to_deck, items):
     for i in range(0, len(items)): 
         move(from_deck, to_deck, items[i])
+
 
 class HanamikojiEngine:
     deck = None
@@ -126,6 +135,7 @@ class HanamikojiEngine:
     player2 = None
     cPlayer = None
     oPlayer = None
+    winner = 0
 
     def __init__(self, name1, name2):
         self.deck = Deck('stack')
@@ -145,7 +155,7 @@ class HanamikojiEngine:
         self.player2.draw(self.deck, 6)
         self.show_table()
 
-    def win(self):
+    def win(self, mode):
         colours = [2,2,2,3,3,4,5]
         points1 = 0
         geishas1 = 0
@@ -174,21 +184,33 @@ class HanamikojiEngine:
         
         if points1 >= 11:  
             print('Player 1 wins')
+            self.winner == 1
         elif points2 >= 11:
             print('Player 2 wins')
+            self.winner == -1
         elif geishas1 >= 4:
             print('Player 1 wins')
+            self.winner == 1
         elif geishas2 >= 4 :
             print('Player 2 wins')
+            self.winner == -1
         else:
             print('No winner yet') 
             round2 = HanamikojiEngine(name_player1, name_player2)
             round2.start()
-            round2.interactive()
+            if mode == 'interactive':
+                round2.interactive()
+            elif mode == 'bot':
+                return round2
             
+
+    def is_game_over(self):
+        print('isgameover?H')
+        return self.winner != 0  
+        
     def interactive(self):
         game_round[0] += 1
-        for i in range(0,num_phases - 1):
+        for i in range(0,num_phases):
             self.cPlayer = self.player1
             self.oPlayer = self.player2
             for j in range(0,2):
@@ -203,63 +225,119 @@ class HanamikojiEngine:
                 self.show_table()
                 self.cPlayer = self.player2
                 self.oPlayer = self.player1
+        self.play_stored()
+        self.show_table()
+        self.win('interactive')
+
+    def play_stored(self):
         print('Play stored cards')
         if self.player1.store.cards != []:
             move(self.player1.store, self.player1.played, self.player1.store.cards[0].id)
         if self.player2.store.cards != []:
             move(self.player2.store, self.player2.played, self.player2.store.cards[0].id)
-        self.show_table()
-        self.win()
+        
 
     def random_move(self):
-        movs = ['Discard', 'Store', 'Trade 3x1', 'Trade 2x2']
+        movs = ['Store', 'Discard', 'Trade 3x1', 'Trade 2x2']
         l = random.randint(0, 3)
-        if movs[l] == 'Discard' and self.cPlayer.moves[0] == 0:
-            select = random.sample(self.cPlayer.hand.cards, 2)
-            print('Discard: {} {}'. format(select[0].id, select[1].id))
-            self.discard(select[0].id, select[1].id)
-            self.cPlayer.moves[0] = 1
         
-        elif movs[l] == 'Store' and self.cPlayer.moves[1] == 0:
+        if movs[l] == 'Store' and self.cPlayer.moves[0][1] == 0:
             select = random.sample(self.cPlayer.hand.cards, 1)
             print('Store: {}'. format(select[0].id))
             self.store(select[0].id)
-            self.cPlayer.moves[1] = 1
+            self.cPlayer.moves[0][1] = 1
         
-        elif movs[l] == 'Trade 3x1' and self.cPlayer.moves[2] == 0:
+        elif movs[l] == 'Discard' and self.cPlayer.moves[1][1] == 0:
+            select = random.sample(self.cPlayer.hand.cards, 2)
+            print('Discard: {} {}'. format(select[0].id, select[1].id))
+            self.discard(select[0].id, select[1].id)
+            self.cPlayer.moves[1][1] = 1
+        
+        elif movs[l] == 'Trade 3x1' and self.cPlayer.moves[2][1] == 0:
             select = random.sample(self.cPlayer.hand.cards, 3)
             print('Trade 3x1: {} {} {}'. format(select[0].id, select[1].id, select[2].id))
             self.trade3x1(select[0].id, select[1].id, select[2].id)
-            self.cPlayer.moves[2] = 1
+            self.cPlayer.moves[2][1] = 1
 
-        elif movs[l] == 'Trade 2x2' and self.cPlayer.moves[3] == 0:   
+        elif movs[l] == 'Trade 2x2' and self.cPlayer.moves[3][1] == 0:   
             select = random.sample(self.cPlayer.hand.cards, 4)
             print('Trade 2x2: {} {} {} {}'. format(select[0].id, select[1].id, select[2].id, select[3].id))
             self.trade2x2(select[0].id, select[1].id, select[2].id, select[3].id)
-            self.cPlayer.moves[3] = 1
+            self.cPlayer.moves[3][1] = 1
         else:
             print('Move already used')
+            
+            print(self.cPlayer.moves)
             self.random_move()
+
+    def monte_carlo(self, action):   #action :[[cards], [move]]            [[['3o', '2p'], ['5b', '3b']], ['Trade2x2', 0, 4]]
+        print(action)
+        if action[1][0] == 'Store' and self.cPlayer.moves[0][1] == 0:
+            self.store(action[0][0])
+            self.cPlayer.moves[0][1] = 1
+
+        elif action[1][0] == 'Discard' and self.cPlayer.moves[1][1] == 0:
+            self.discard(action[0][0], action[0][1])
+            self.cPlayer.moves[1][1] = 1
+           
+        elif action[1][0] == 'Trade 3x1' and self.cPlayer.moves[2][1] == 0:
+            self.trade3x1(action[0][0], action[0][1], action[0][2])
+            self.cPlayer.moves[2][1] = 1
+
+        elif action[1][0] == 'Trade 2x2' and self.cPlayer.moves[3][1] == 0:   
+            self.trade2x2(action[0][0][0], action[0][0][1], action[0][1][0], action[0][1][1])
+            self.cPlayer.moves[3][1] = 1
+        else:
+            print('Move already used')
+
+        #self.show_table()
+        self.switch_players()
+
+        if self.turn_over():
+            self.win('bot')
+
+        self.cPlayer.draw(self.deck, 1)
+
+    def switch_players(self):
+        if self.cPlayer == self.player1:
+            self.cPlayer = self.player2
+            self.oPlayer = self.player1
+        else:
+            self.cPlayer = self.player1
+            self.oPlayer = self.player2
+
+    def turn_over(self):
+        counter = 0
+        for move in self.cPlayer.moves:
+            if move[1] == 1:
+                counter += 1
+        
+        for move in self.oPlayer.moves:
+            if move[1] == 1:
+                counter += 1
+        
+        is_over = (counter == 8)
+        return is_over
 
     def redirect(self, inpt):
         if inpt == 'Random':
             self.random_move()
-
-        elif inpt == 'Discard' and self.cPlayer.moves[0] == 0:
-            self.discard(input('First discard: '), input('Second discard: '))
-            self.cPlayer.moves[0] = 1
-           
-        elif inpt == 'Store' and self.cPlayer.moves[1] == 0:
+                   
+        elif inpt == 'Store' and self.cPlayer.moves[0][1] == 0:
             self.store(input('Store: '))
-            self.cPlayer.moves[1] = 1
-           
-        elif inpt == 'Trade 3x1' and self.cPlayer.moves[2] == 0:
-            self.trade3x1(input('First trade: '), input('Second trade: '), input('Third trade: '))
-            self.cPlayer.moves[2] = 1
+            self.cPlayer.moves[0][1] = 1
 
-        elif inpt == 'Trade 2x2' and self.cPlayer.moves[3] == 0:   
+        elif inpt == 'Discard' and self.cPlayer.moves[1][1] == 0:
+            self.discard(input('First discard: '), input('Second discard: '))
+            self.cPlayer.moves[1][1] = 1
+           
+        elif inpt == 'Trade 3x1' and self.cPlayer.moves[2][1] == 0:
+            self.trade3x1(input('First trade: '), input('Second trade: '), input('Third trade: '))
+            self.cPlayer.moves[2][1] = 1
+
+        elif inpt == 'Trade 2x2' and self.cPlayer.moves[3][1] == 0:   
             self.trade2x2(input('First pair: '), input('- '), input('Second pair: '), input('- '))
-            self.cPlayer.moves[3] = 1
+            self.cPlayer.moves[3][1] = 1
         else:
             print('Move already used')
                 
@@ -332,14 +410,227 @@ else:
     name_player2 = input('Enter Player 2 name:')
 game = HanamikojiEngine(name_player1, name_player2)
 game.start()
-game.interactive()
+game.cPlayer.draw(game.deck, 1)
+#game.interactive()
+
+def get_legal_actionss(games): 
+        legal_actions = []
+        for m in games.cPlayer.moves:
+            if m[1] == 0:
+                if m[0] == 'Trade2x2':
+                    cCards = games.cPlayer.hand.deck_id()
+                    combi = list(set(combinations(cCards, m[2])))
+                    pairs = []
+                    double_pairs = []
+                    for comb in combi:
+                        pairs.append(list(set(combinations(comb, 2))))
+                    for i in range(0,len(pairs)):
+                        for pair in pairs[i]:
+                            k = set(combi[i]) - set(pair) 
+                            double_pair = [list(k), list(pair)]
+                            double_pairs.append(double_pair)
+
+                    for d_pair in double_pairs :
+                        legal_actions.append([d_pair, m])
+
+                else:
+                    cCards = games.cPlayer.hand.deck_id()
+                    combi = list(set(combinations(cCards, m[2])))
+                    for mov in combi :
+                        legal_actions.append([list(mov), m])
+                #find move, do all play combinations
+            else:
+                pass 
+            print(legal_actions)
+            print(len(legal_actions))
 
 
+#monte carlo tree search
+
+import numpy as np
+from collections import defaultdict
+
+class MonteCarloTreeSearchNode():
+    def __init__(self, state, parent=None, parent_action=None):       #sets defauults?
+        print('new node')
+        self.state = state   #Class:Hanamikoji Engine
+        self.parent = parent   #Class:Hanamikoji Engine
+        self.parent_action = parent_action     #[[cards],[move]]
+        self.children = []      #Class: list of Hanamikoji Engine
+        self._number_of_visits = 0
+        self._results = defaultdict(int)
+        self._results[1] = 0
+        self._results[-1] = 0
+        self._untried_actions = None       #Class: list [[],[]]
+        self._untried_actions = self.untried_actions()
+        return
+
+    def untried_actions(self):
+        print('untried actions')
+        self._untried_actions = self.get_legal_actions()
+        return self._untried_actions
+
+    def n(self):
+        print('n')
+        return self._number_of_visits
+
+    def expand(self):
+        print('expand open')
+        action = self._untried_actions.pop()
+        next_state = self.move(action)
+        next_state.show_table()
+        child_node = MonteCarloTreeSearchNode(next_state, parent=self, parent_action=action)
+        self.children.append(child_node)
+        print('expand close')
+        return child_node 
+
+    def is_terminal_node(self):
+        print('is terminal')
+        return self.is_game_over()
+
+    def rollout(self):
+        print('rollout')
+        current_rollout_state = self
+
+        while not current_rollout_state.is_game_over():
+            possible_moves = current_rollout_state.get_legal_actions()
+            action = self.rollout_policy(possible_moves)
+            current_rollout_state.state = current_rollout_state.move(action)
+        print('rollout close')
+        return current_rollout_state.game_result()
+
+    def backpropagate(self, result):
+        print('backpropagate')
+        self._number_of_visits += 1.
+        self._results[result] += 1.
+        if self.parent:
+            self.parent.backpropagate(result)
+
+    def is_fully_expanded(self):
+        print('isfullyexpanded')
+        return len(self._untried_actions) == 0
+
+    def best_child(self, c_param=0.1):
+        print('bestchild')
+        choices_weights = [(c.q() / c.n()) + c_param * np.sqrt((2 * np.log(self.n()) / c.n())) for c in self.children]
+        return self.children[np.argmax(choices_weights)]
+
+    def rollout_policy(self, possible_moves):
+        print('rollout policy')
+        return possible_moves[np.random.randint(len(possible_moves))]
+
+    def _tree_policy(self):
+        print('tree policy')
+        current_node = self
+        while not current_node.is_terminal_node():
+            if not current_node.is_fully_expanded():
+                return current_node.expand()
+            else:
+                current_node = current_node.best_child()
+        return current_node
+
+    def best_action(self):
+        print('bestaction')
+        simulation_no = 10
+
+        for i in range(simulation_no):
+            v = self._tree_policy()
+            reward = v.rollout()
+            v.backpropagate(reward)
+        
+        return self.best_child(c_param=0.)
+
+    def get_legal_actions(self): 
+            print('getlegalactions')
+            legal_actions = []
+            for m in self.state.cPlayer.moves:
+                if m[1] == 0:
+                    if m[0] == 'Trade 2x2':
+                        cCards = self.state.cPlayer.hand.deck_id()
+                        combi = list((combinations(cCards, m[2])))
+                        pairs = []
+                        double_pairs = []
+                        for comb in combi:
+                            pairs.append(list(set(combinations(comb, 2))))
+                        for i in range(0,len(pairs)):
+                            for pair in pairs[i]:
+                                k = list(combi[i])
+                                k.remove(pair[0])
+                                k.remove(pair[1])
+                                #k = set(combi[i]) - set(pair) 
+                                double_pair = [k, list(pair)]
+                                double_pairs.append(double_pair)
+
+                        for d_pair in double_pairs :
+                            legal_actions.append([d_pair, m])
+
+                    else:
+                        cCards = self.state.cPlayer.hand.deck_id()
+                        combi = list(set(combinations(cCards, m[2])))
+                        for mov in combi :
+                            legal_actions.append([list(mov), m])
+                    #find move, do all play combinations
+                else:
+                    pass 
+                #print(legal_actions)
+            print(len(legal_actions))
+            return legal_actions
+
+            #Modify according to your game or needs. Constructs a list of all possible states from current state.
+            #Returns a list.from current player get all actions left
+
+
+    def is_game_over(self):
+        print('isgameover?')
+        print(self.state.winner)
+        return self.state.winner != 0 
+
+    def game_result(self):
+        print('gameresult')
+        return self.state.winner
+
+    def move(self,action):  #action: [[],[]]
+        print('move')
+        new_state = copy.deepcopy(self.state)
+        new_state.monte_carlo(action)
+
+        return new_state
+
+        #return: state: game.copy
+        #self.state = copy.deepcopy(action)
+
+        '''
+        -previos state: game
+        - action [[['3o', '2p'], ['5b', '3b']], ['Trade2x2', 0, 4]]   [[cards],[name]]
+        Modify according to your game or 
+        needs. 
+        
+        Changes the state of your board with a new value. 
+        
+        or a normal Tic Tac Toe game, it can be a 3 by 3
+        array with all the elements of array
+        being 0 initially. 0 means the board 
+        position is empty. If you place x in
+        row 2 column 3, then it would be some 
+        thing like board[2][3] = 1, where 1
+        represents that x is placed. Returns 
+        the new state after making a move.
+        '''
+
+state = copy.deepcopy(game)
+
+def main():
+        root = MonteCarloTreeSearchNode(state,None)
+        print('done')
+        print(root)
+        selected_node = root.best_action()
+        return 
+
+main()
 # 2p, 2r, 2y, 3b, 3o, 4g, 5b
 
 #print('---------------------------1----------------------------------------------')
 #print('~~~~~~~~~~~~~~~~~~~~~~~~~~2~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
 #print('~~~~~~~~~~~~~~~~~~~~~~~~~~3~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
 #Back: RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE
-
 
